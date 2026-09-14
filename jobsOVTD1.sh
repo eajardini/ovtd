4d3ea9f524cc6d8d96259b50ac7da78cc95771ae
Evandro Jardini <eajardini@gmail.com>
2026-09-14 08:51:11 -0300

    Update script path for kitchen.sh execution

--- jobsOVTD1.sh
+++ jobsOVTD1.sh
@@ -1,4 +1,4 @@
 # caminho_do_kitchen/kitchen.sh -file: "caminho_do_job"
 
-sh /opt/install/pentaho/data-integration/kitchen.sh -file:"/home/eajardini/Dropbox/apostilas/posgrad/ifsp/scripts/capituloETL/bi_OVDT1.kjb"
+sh /opt/install/pentaho/data-integration/kitchen.sh -file:"/Caminho_completo_a_partir_raiz/bi_OVDT1.kjb"
 
